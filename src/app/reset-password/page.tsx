6ffd1f8cd53f9a9542e3aa64329b6ccbc996104a
Rishi-Sidharda <rishisidharda@gmail.com>
2025-08-18 00:44:29 +0530

    reset-password page modified

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+// ADD THE DEFAULT URL IN FIREBASE AUTHENTICATION RESET-PASSWORD TEMPLATE
+
 import { useState, useEffect, FormEvent } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { auth } from "@/lib/firebase";
