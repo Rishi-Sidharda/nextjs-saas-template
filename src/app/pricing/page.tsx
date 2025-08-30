71d94d904fe41b19c01878b332faab7e95e9fd20
Rishi-Sidharda <rishisidharda@gmail.com>
2025-08-30 12:29:49 +0530

    Initial commit

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -3,9 +3,22 @@
 import { useState } from "react";
 import { plans } from "./plans-config";
 import { PricingTableTwo } from "@/components/billingsdk/pricing-table-two";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "@/lib/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import router from "next/router";
 
 export default function Pricing() {
-  const [isAnnually, setIsAnnually] = useState(false);
+  const [user, loading] = useAuthState(auth);
+
+  function actionButton(planId: any, billingCycle: any) {
+    if (user) {
+      alert(planId);
+      alert(billingCycle);
+    } else {
+      router.push("/login");
+    }
+  }
 
   return (
     <div className="bg-white">
@@ -14,7 +27,9 @@ export default function Pricing() {
         className="w-full max-w-4xl mx-auto"
         title={`We offer ${plans.length} plans`}
         description="Choose the plan that's right for you"
-        onPlanSelect={(planId, billingCycle) => alert(billingCycle)}
+        onPlanSelect={(planId, billingCycle) =>
+          actionButton(planId, billingCycle)
+        }
         size="small" // small, medium, large
         theme="classic" // minimal or clas
         // sic
