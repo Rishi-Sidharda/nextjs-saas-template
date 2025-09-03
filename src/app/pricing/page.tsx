f8f1e379dde025803182e32c1b3e0341bd2060d1
Rishi-Sidharda <rishisidharda@gmail.com>
2025-09-03 15:22:15 +0530

    Initial commit from Create Next App

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import { plans } from "./plans-config";
+import { PricingTableOne } from "@/components/billingsdk/pricing-table-one";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "@/lib/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useRouter } from "next/navigation";
+
+export default function Pricing() {
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
+
+  async function actionButton(planTitle: string, billingCycle: string) {
+    if (!user) {
+      router.push("/login");
+      return;
+    }
+
+    // Map your plan names + billing cycle to Dodo plan IDs
+  }
+
+  return (
+    <div className="bg-white">
+      <PricingTableOne
+        plans={plans}
+        className="w-full max-w-4xl mx-auto"
+        title={`We offer ${plans.length} plans`}
+        description="Choose the plan that's right for you"
+        onPlanSelect={(planTitle, billingCycle) =>
+          actionButton(planTitle, billingCycle)
+        }
+        size="medium" // small, medium, large
+        theme="classic" // minimal or clas
+        // sic
+      />
+    </div>
+  );
+}
